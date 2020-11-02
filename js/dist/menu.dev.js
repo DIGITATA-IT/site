8e574867b9fbe0edb82c4e2ff39af8610ced6799
Maurice <62309708+MomoD92@users.noreply.github.com>
2020-11-02 15:43:06 +0100

    my first commit

---
+++ js/dist/menu.dev.js
@@ -0,0 +1,8 @@
+"use strict";
+
+$(document).ready(function () {
+  $('.menuToggle').click(function () {
+    $('.headerNav').stop(true, true).slideToggle();
+  });
+});
+//# sourceMappingURL=menu.dev.js.map
